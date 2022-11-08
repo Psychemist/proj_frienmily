7d7026aef62343b421edc7e6181e7944cef25497
Louie Chan <lokyin0831@gmail.com>
2022-11-08 18:27:01 +0800

    add button in friend page

---
+++ Frienmily/components/FriendItem.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import { Icon } from 'react-native-elements'
+
+export default function FriendItem() {
+
+
+
+    return (
+        <View>
+            <Text>hello</Text>
+        </View>
+    )
+}
