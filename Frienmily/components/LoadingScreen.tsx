cedd342379fb15ff0b50666065d9670327905bcc
Mike Chan <kiwai0929@gmail.com>
2022-11-18 18:34:32 +0800

    complete the logic of isLoggedIn

--- Frienmily/components/LoadingScreen.tsx
+++ Frienmily/components/LoadingScreen.tsx
@@ -3,30 +3,36 @@ import { StyleSheet, Text, View } from 'react-native'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
 import { useNavigation } from '@react-navigation/native'
+import userSlice, { reLogin } from '../redux/user/userSlice'
+import { getStoredAuth } from '../redux/user/thunk'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 
 export default function LoadingScreen() {
     const navigation = useNavigation()
     const isLoggedIn = useSelector((state: RootState) => state.user.isLoggedIn)
+    const userState = useSelector((state: RootState) => state.user)
+    const dispatch = useDispatch()
 
     useEffect(() => {
-        const intervalId = setTimeout(() => {
-            if (isLoggedIn) {
-                navigation.navigate('Groups' as never)
-                console.log("isLoggedIn is true at LoadingScreen. Navigated to Homepage(Groups Page).")
+        const checkLogin = async () => {
+            const token = await AsyncStorage.getItem('token')
 
-            } else {
-                navigation.navigate('Login' as never)
-                console.log("isLoggedIn is false at LoadingScreen. Navigated to LoginScreen.")
-            }
+            const intervalId = setTimeout(() => {
+                if (token) {
+                    dispatch(reLogin(token))
+                    navigation.navigate('HomeTab' as never)
+                    console.log("isLoggedIn is true at LoadingScreen. Navigated to Homepage(Groups Page).")
+                } else {
+                    navigation.navigate('Login' as never)
+                    console.log("isLoggedIn is false at LoadingScreen. Navigated to LoginScreen.")
+                }
+            }, 1100)
 
-        }, 1300)
-        return () => {
-            if (intervalId) {
-                clearInterval(intervalId)
-            }
         }
-    }, [])
+        checkLogin()
+
+    }, [isLoggedIn])
 
     const styles = StyleSheet.create({
         body: {
