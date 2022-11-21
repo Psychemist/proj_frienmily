ea5bcee09b6881283088e5a0b44e86eb9d159184
Mike Chan <kiwai0929@gmail.com>
2022-11-21 15:18:56 +0800

    fixed the warning msg of @env

--- Frienmily/babel.config.js
+++ Frienmily/babel.config.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     ["module:react-native-dotenv", {
       "envName": "APP_ENV",
-      "moduleName": "myenv",
+      "moduleName": "@env",
       "path": ".env",
       "safe": false,
       "allowUndefined": true,
