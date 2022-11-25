d39fa95ff1570d062912c0b4ef504fc8744cd00f
Ronson Kwok <rk6866@hotmail.com>
2022-11-25 16:25:58 +0800

    amended CSS - Create Group

--- Frienmily/components/FriendItemInCreateGroup.tsx
+++ Frienmily/components/FriendItemInCreateGroup.tsx
@@ -12,33 +12,47 @@ export default function FriendItemInCreateGroup(props: FriendItemInCreateGroupPr
 
     const styles = StyleSheet.create({
         itemContainer: {
-            width: "100%",
-            backgroundColor: "pink",
+            width: "90%",
+            // height: "40%",
+            backgroundColor: props.items.isSelected ? "#47b4b1" : "white",
             alignItems: "center",
             flexDirection: "row",
-            padding: 10,
-            paddingTop: 20,
-            paddingBottom: 20,
+            justifyContent: "space-around",
+            padding: 20,
+            // paddingTop: 20,
+            // paddingBottom: 20,
             borderRadius: 15,
-            marginTop: 5,
+            // marginTop: 5,
             marginBottom: 5,
-            borderColor: props.items.isSelected ? "black" : "pink",
+            borderColor: props.items.isSelected ? "#47b4b1" : "white",
             borderWidth: 3,
+            shadowColor: props.items.isSelected ? "lightgray": "lightgray",
+            shadowOpacity: 3,
+            shadowRadius: 3,
+            shadowOffset: {
+              height: props.items.isSelected ? 4 : 0,
+              width: props.items.isSelected ? 4 : 0,
+            },
         },
         userImage: {
-            width: 60,
-            height: 60,
+            width: 50,
+            height: 50,
             borderRadius: 50,
             backgroundColor: 'grey',
-            marginRight: 20
+            // marginRight: 20
         },
+        userNameText:{
+            fontSize: 17,
+            color: props.items.isSelected ? "white" : "gray",
+    
+        }
     })
     const boo = true
     return (
         <View>{props.items.isShow ? <TouchableOpacity style={styles.itemContainer} onPress={() => { props.itemPress(props.arrayIndex) }}>
             <Image style={styles.userImage} source={{ uri: props.items.profile_picture }} ></Image>
 
-            <Text>     {props.items.username}</Text>
+            <Text style={styles.userNameText}>     {props.items.username}</Text>
         </TouchableOpacity> : null}</View>
 
     )
