d00ad4af438f879180e690c5b9676a571d86854c
Ronson Kwok <rk6866@hotmail.com>
2022-11-28 15:58:47 +0800

    多間同價

--- Frienmily/components/GroceriesRandomItems.tsx
+++ Frienmily/components/GroceriesRandomItems.tsx
@@ -12,23 +12,93 @@ export default function GroceriesRandomItems(props: GroceriesRandomItemsProps) {
 
     const navigation = useNavigation();
     const getLowest = () => {
+
+        let wellcome_price
+        if (props.item.wellcome_price == null) {
+            wellcome_price = null
+        } else {
+            wellcome_price = parseFloat(props.item.wellcome_price)
+        }
+        let parknshop_price
+        if (props.item.parknshop_price == null) {
+            parknshop_price = null
+        } else {
+            parknshop_price = parseFloat(props.item.parknshop_price)
+        }
+        let jasons_price
+        if (props.item.jasons_price == null) {
+            jasons_price = null
+        } else {
+            jasons_price = parseFloat(props.item.jasons_price)
+        }
+        let watsons_price
+        if (props.item.watsons_price == null) {
+            watsons_price = null
+        } else {
+            watsons_price = parseFloat(props.item.watsons_price)
+        }
+        let mannings_price
+        if (props.item.mannings_price == null) {
+            mannings_price = null
+        } else {
+            mannings_price = parseFloat(props.item.mannings_price)
+        }
+        let aeon_price
+        if (props.item.aeon_price == null) {
+            aeon_price = null
+        } else {
+            aeon_price = parseFloat(props.item.aeon_price)
+        }
+        let dch_price
+        if (props.item.dch_price == null) {
+            dch_price = null
+        } else {
+            dch_price = parseFloat(props.item.dch_price)
+        }
+        let ztore_price
+        if (props.item.ztore_price == null) {
+            ztore_price = null
+        } else {
+            ztore_price = parseFloat(props.item.ztore_price)
+        }
+
         let allPriceArray = [
-            { price: parseFloat(props.item.wellcome_price), shop: "惠康" },
-            { price: parseFloat(props.item.parknshop_price), shop: "百佳" },
-            { price: parseFloat(props.item.jasons_price), shop: "Jasons" },
-            { price: parseFloat(props.item.watsons_price), shop: "屈臣氏" },
-            { price: parseFloat(props.item.mannings_price), shop: "萬寧" },
-            { price: parseFloat(props.item.aeon_price), shop: "AEON" },
-            { price: parseFloat(props.item.dch_price), shop: "大昌食品" },
-            { price: parseFloat(props.item.ztore_price), shop: "士多" }
+            { price: wellcome_price, shop: "惠康" },
+            { price: parknshop_price, shop: "百佳" },
+            { price: jasons_price, shop: "Jasons" },
+            { price: watsons_price, shop: "屈臣氏" },
+            { price: mannings_price, shop: "萬寧" },
+            { price: aeon_price, shop: "AEON" },
+            { price: dch_price, shop: "大昌食品" },
+            { price: ztore_price, shop: "士多" }
         ]
+
+        console.log("props.item :", props.item)
+
+        
+        
         let filtered = allPriceArray.filter(function (e) {
-            return e.price != NaN;
+            return e.price != null;
         });
+        console.log("filtered :", filtered)
+
         const lowest = filtered.reduce((previous, current) => {
-            return current.price < previous.price ? current : previous;
+            return current.price! < previous.price! ? current : previous;
         });
+        let tempArray = []
+        for (let item of filtered) {
+            if (item.price == lowest.price) {
+                tempArray.push(item)
+            }
+        }
+        console.log(tempArray)
+        if (tempArray.length > 1) {
+            console.log({"price": lowest.price, "shop": "多間同價"})
+            return {"price": lowest.price, "shop": "多間同價"}
+        }
+        console.log("lowest :", lowest)
         return lowest
+
     }
 
     const styles = StyleSheet.create({
