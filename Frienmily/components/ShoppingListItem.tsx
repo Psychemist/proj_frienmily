9d0778cabe35c7bc344f0f1ae1ee18a28ea770f8
Ronson Kwok <rk6866@hotmail.com>
2022-11-29 18:02:21 +0800

    commit

--- Frienmily/components/ShoppingListItem.tsx
+++ Frienmily/components/ShoppingListItem.tsx
@@ -292,9 +292,9 @@ export default function ShoppingListItem(props: ShoppingListItemProps) {
                 navigation.navigate('ImagePreview' as never, { image: props.items.goods_picture } as never)
             }}>
                 <View><Image source={{ uri: props.items.goods_picture }}
-                    style={{ width: 50, height: 50 }} /></View>
+                    style={{ width: 50, height: 50, marginRight:"2%"}} /></View>
             </TouchableOpacity>
-            <View style={{ width: 200 }}>
+            <View style={{ width: 150 }}>
                 <View><Text style={styles.text}>{props.items.name}</Text></View>
                 <View><Text style={styles.shopText}>{getLowest().shop}</Text></View>
                 <View><Text style={styles.shopText}>Item added by {assigneeName}</Text></View>
