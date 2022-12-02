57e30656dcb3a24b991f607e0dcf1ea19b2312e1
Mike Chan <kiwai0929@gmail.com>
2022-12-02 13:52:59 +0800

    change the code related to env

--- Frienmily/components/SignUpScreen.tsx
+++ Frienmily/components/SignUpScreen.tsx
@@ -31,7 +31,7 @@ export default function SignUpScreen() {
   const onSignUp = async () => {
     try {
       console.log({ username, password, mobileNumber, email })
-      const res = await fetch('http://localhost:8000/user/register', {
+      const res = await fetch(`${process.env.REACT_APP_API_SERVER}/user/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
